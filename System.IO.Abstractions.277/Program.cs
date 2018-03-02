29eeb575e3fcc6e9b581ab4ee457991806c771ad
David Fallah <davidfallah@atp-group.com>
2018-03-02 08:37:03 +0000

    Add project files.

---
+++ System.IO.Abstractions.277/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System.IO.Abstractions._277
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
